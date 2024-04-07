3c3e8741bd9b15fe9763525437690b47ca1dc057
Martina Esposito <martina.esposito@mail.polimi.it>
2024-04-07 20:27:54 +0200

    program almost stylized :))))

---
+++ incontri.js
@@ -0,0 +1,119 @@
+let incontri = [
+    {
+      day: "1", //tuesday
+      time: "18.30-22.00",
+      title: "Grand opening",
+      name: "",
+      description: "",
+    },
+
+
+    {
+      day: "2",//wednesday
+      time: "11.30-12.30",
+      title: "Material Intelligence_ Beyond Image.",
+      name: "Tania Winkler",
+      description:
+        "With both AI and Material Intelligence rooted in collective memory, this year superMATTER RCA Platform proposes materiality as container (repository) and signs of human capital to explore the incursion of new technologies in the production of new spatial identities.",
+    },
+    {
+      day: "2",
+      time: "14.30-15.30",
+      title:
+        "Design and design schools in/for the ecological transition. </br> Care, proximity, empowering, communing: four keywords to orient design education. ",
+      name: "Ezio Manzini",
+      description:
+        "How does the RCA Interior Design Programme engage with the challenges we are currently facing in the world? Climate Curriculums describes thinking and work that relates to climatic and socially based challenges.",
+    },
+    {
+      day: "2", 
+      time: "17.00-18.00",
+      title: "Climate Curriculum(s)",
+      name: "Graeme Brooker",
+      description:  "How does the RCA Interior Design Programme engage with the challenges we are currently facing in the world? Climate Curriculums describes thinking and work that relates to climatic and socially based challenges.",
+    },
+
+
+    {
+      day: "3",//thursday
+      time: "12.00-13.00",
+      title: "Enabling circularity and clean energy with thermochemical waste valorization and upcycling: a game of interdependencies",
+      name: "Matteo Pelucchi",
+      description:"Thermochemical conversion of waste and its integration with renewable energy and value chains of the chemical industry is expected to play a key role in production of carbon free energy carriers and chemicals.",
+    },
+    {
+      day: "3",
+      time: "14.00-15.30",
+      title:"Alimentazione del Futuro: verso un Mondo Sostenibile",
+      name: "Marita Canina & students",
+      description: "Un talk a più voci del Dipartimento per il Future Food del Ministero per il Futuro. Unisciti a noi mentre immaginiamo insieme un futuro in cui il cibo non solo nutre il corpo, ma anche il pianeta. </br>  [event in Italiano]",
+    },
+    {
+      day: "3", 
+      time: "18.00-19.00",
+      title: "Zen and Design",
+      name: "Master Jiqun",
+      description: "Buddist master Jiqun meets students, scholars and designers.",
+    },
+
+    {
+        day: "4",//friday
+        time: "14.30-15.00",
+        title: "Design education in Japan: interdisciplinarity",
+        name: "Setsu Ito, University of Tsukuba, Japan",
+        description:"A flexible education and research structure and a university system thought to meet the needs of the next generation.",
+      },
+      {
+        day: "4",
+        time: "15.00-15.30",
+        title:"Design education in Japan: nature centricity.",
+        name: "Setsu Ito, University of Tsukuba, Japan",
+        description: "Japan's original science, technology, art and design for the creation of a sustainable and inclusive society.",
+      },
+      {
+        day: "4", 
+        time: "15.30-16.30",
+        title: "Moving minds with objects",
+        name: "Odo Fioravanti",
+        description: "The industrial design practice as a researching tool.",
+      }, 
+      {
+        day: "4", 
+        time: "17.00-18.00",
+        title: "Esoeditoria per l’innovazione sociale",
+        name: "Elena Caratti",
+        description: "Social Indie Magazines come espressione dell’interdipendenza fra aree di ricerca, contenuti, codici e il contesto socio-politico.",
+      },
+
+
+      {
+        day: "5",//saturday
+        time: "11.00-13.00",
+        title:"Where Do We Go From Here",
+        name: "Campo Testrale",
+        description: "",
+      },
+      {
+        day: "5",
+        time: "15.00-17.00",
+        title:"Where Do We Go From Here",
+        name: "Campo Testrale",
+        description: "",
+      },
+      {
+        day: "5", 
+        time: "18.00-20.00",
+        title:"Where Do We Go From Here",
+        name: "Campo Testrale",
+        description: "",
+      }, 
+
+
+      {
+        day: "6",//sunday
+        time: "11.00-13.00",
+        title:"Where Do We Go From Here",
+        name: "Campo Testrale",
+        description: "",
+      },
+  ];
